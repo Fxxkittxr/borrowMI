8d6b7d1321f71dcb0d3f0c2c60d457a845017f0c
PALOMA TEIXEIRA <paloma.teixeira@univille.br>
2024-09-10 19:52:53 -0300

    first commit

---
+++ src/main/java/br/univille/fsoweb20242borrowmi/Fsoweb20242borrowmiApplication.java
@@ -0,0 +1,13 @@
+package br.univille.fsoweb20242borrowmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsoweb20242borrowmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsoweb20242borrowmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/fsoweb20242borrowmi/Fsoweb20242borrowmiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.fsoweb20242borrowmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsoweb20242borrowmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
